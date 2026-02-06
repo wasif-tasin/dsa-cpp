37b8f5715cd99b6c1f3ec36ac6a3e3640b9239d3
wasif-tasin <dsnmdwasif@gmail.com>
2026-02-07 02:45:28 +0600

    Added stack implementations in C++

---
+++ stack_input.cpp.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+class mystack
+{
+public:
+    vector<int> v;
+    void push(int val)
+    {
+        v.push_back(val);
+    }
+    void pop()
+    {
+        v.pop_back();
+    }
+    int top()
+    {
+        return v.back();
+    }
+    int size()
+    {
+        return v.size();
+    }
+    bool empty()
+    {
+        return v.empty();
+    }
+};
+int main()
+{
+    mystack st;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        st.push(x);
+    }
+    while (!st.empty())    // !st.empty() that mean not empty = st.empty() == false;
+    {
+        cout << st.top() << endl;
+        st.pop();
+    }
+    return 0;
+}
