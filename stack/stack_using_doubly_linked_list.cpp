b72f0ae09ca2860bd2d5743c642491610f943d59
wasif-tasin <dsnmdwasif@gmail.com>
2026-02-07 03:20:22 +0600

    Added stack implementations in C++

---
+++ stack/stack_using_doubly_linked_list.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+class mystack
+{
+public:
+    Node *head = NULL;
+    Node *tail = NULL;
+    int sz = 0;
+
+    void push(int val)
+    {
+        sz++;
+        Node *newnode = new Node(val);
+        if (head == NULL)
+        {
+            head = newnode;
+            tail = newnode;
+            return;
+        }
+        tail->next = newnode;
+        newnode->prev = tail;
+        tail = newnode;
+    }
+    void pop()
+    {
+        sz--;
+        Node *deletenode = tail;
+        tail = tail->prev;
+        delete deletenode;
+        if (tail == NULL)
+        {
+            head = NULL;
+            return;
+        }
+        tail->next = NULL;
+    }
+    int top()
+    {
+        return tail->val;
+    }
+    int size()
+    {
+        return sz;
+    }
+    bool empty()
+    {
+        //     if(head == NULL)
+        //     return true;
+        //     else
+        //     return false;
+        return head == NULL;
+    }
+};
+int main()
+{
+    mystack st;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        st.push(x);
+    }
+
+    while (!st.empty()) // !st.empty() that mean not empty = st.empty() == false
+    {
+    cout << st.top() << endl;
+        st.pop();
+    }
+
+    return 0;
+}
