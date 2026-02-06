30dee315228948d08f5c76d58715c3f7a90e8552
wasif-tasin <dsnmdwasif@gmail.com>
2026-02-07 03:09:08 +0600

    Update basic_queue_problems.cpp.cpp

--- queue/basic_queue_problems.cpp.cpp
+++ queue/basic_queue_problems.cpp.cpp
@@ -1,173 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Node
-{
-public:
-    int val;
-    Node *next;
-    Node *prev;
-    Node(int val)
-    {
-        this->val = val;
-        this->next = NULL;
-        this->prev = NULL;
-    }
-};
 
-class mystack
-{
-public:
-    Node *head = NULL;
-    Node *tail = NULL;
-    int sz = 0;
+/*
+    File: basic_queue_problems.cpp
+    Purpose: Demonstrates basic queue operations through a simple simulation.
+    Concepts used: push, pop, front, empty
+*/
 
-    void push(int val)
-    {
-        sz++;
-        Node *newnode = new Node(val);
-        if (head == NULL)
-        {
-            head = newnode;
-            tail = newnode;
-            return;
-        }
-        tail->next = newnode;
-        newnode->prev = tail;
-        tail = newnode;
-    }
-    void pop()
-    {
-        sz--;
-        Node *deletenode = tail;
-        tail = tail->prev;
-        delete deletenode;
-        if (tail == NULL)
-        {
-            head = NULL;
-            return;
-        }
-        tail->next = NULL;
-    }
-    int top()
-    {
-        return tail->val;
-    }
-    int size()
-    {
-        return sz;
-    }
-    bool empty()
-    {
-        //     if(head == NULL)
-        //     return true;
-        //     else
-        //     return false;
-        return head == NULL;
-    }
-};
+int main() {
+    queue<int> q;
 
-class myQueue
-  {
-    public:
-    Node* head = NULL;
-    Node* tail = NULL;
-    int sz = 0;
+    q.push(10);
+    q.push(20);
+    q.push(30);
 
-    void push (int val)
-    {
-        sz++;
-        Node* newnode = new Node(val);
-        if(head == NULL)
-        {
-            head = newnode;
-            tail = newnode;
-            return;
-        }
-        tail->next = newnode;
-        newnode->prev = tail;
-        tail = newnode;
-    }
-     void pop()
-     {
-        sz--;
-        Node* deletenode = head;
-        head = head->next;
-        delete deletenode;
-        if(head == NULL)
-        {
-            tail = NULL;
-            return;
-        }
-        head->prev=NULL;
-     }
-     int front()
-     {
-        return head->val;
-     }
-     
-     int back()
-     {
-        return tail->val;
-     }
+    cout << "Front element: " << q.front() << endl;
 
-     int size()
-     {
-        return sz;
-     }
+    q.pop();
 
-     bool empty()
-     {
-        return head == NULL;
-     }
+    cout << "Front element after pop: " << q.front() << endl;
 
-  };
+    cout << "Queue elements: ";
+    while (!q.empty()) {
+        cout << q.front() << " ";
+        q.pop();
+    }
+    cout << endl;
 
-
-int main()
-{
-    mystack st;
-    myQueue q;
-    int n,m;
-    cin >> n >> m;
-    for (int i = 0; i < n; i++)
-    {
-        int x;
-        cin >> x;
-        st.push(x);
-    }
-     for (int i = 0; i < m; i++)
-    {
-        int val;
-        cin >> val;
-        q.push(val);
-    }
-    if(st.size() != q.size())
-    {
-        cout << "NO";
-        return 0;
-    }
-    while (!st.empty())
-    {
-        if (st.top() == q.front())
-        {
-            st.pop();
-            q.pop();
-        }
-        else
-        {
-            cout << "NO";
-            break;
-        }
-    
-    }
-
-    if (st.empty() && q.empty())
-    {
-        cout << "YES";
-    }
-    else if(st.empty() && q.empty() == false)
-    {
-        cout << "NO";
-    }
     return 0;
 }
