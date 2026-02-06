15492c17528fd03e2621faaff232ef4c6349a8e9
wasif-tasin <dsnmdwasif@gmail.com>
2026-02-07 03:20:48 +0600

    Added queue implementations in C++

---
+++ queue/queue_using_singly_linked_list.cpp
@@ -0,0 +1,85 @@
+  #include<bits/stdc++.h>
+  using namespace std;
+  class Node
+  {
+      public:
+          int val;
+          Node* next;
+      Node(int val)
+      {
+          this->val = val;
+          this->next = NULL;
+      }
+  };
+
+  class myQueue
+  {
+    public:
+    Node* head = NULL;
+    Node* tail = NULL;
+    int sz = 0;
+
+    void push (int val)
+    {
+        sz++;
+        Node* newnode = new Node(val);
+        if(head == NULL)
+        {
+            head = newnode;
+            tail = newnode;
+            return;
+        }
+        tail->next = newnode;
+        tail = newnode;
+    }
+     void pop()
+     {
+        sz--;
+        Node* deletenode = head;
+        head = head->next;
+        delete deletenode;
+        if(head == NULL)
+        {
+            tail == NULL;
+        }
+     }
+     int front()
+     {
+        return head->val;
+     }
+     
+     int back()
+     {
+        return tail->val;
+     }
+
+     int size()
+     {
+        return sz;
+     }
+
+     bool empty()
+     {
+        return head == NULL;
+     }
+
+  };
+  int main()
+  {
+    myQueue q;
+    int n;
+    cin>>n;
+    for(int i=0; i<n; i++)
+    {
+        int val;
+        cin>>val;
+        q.push(val);
+    }
+    while(! q.empty())
+    {
+        cout << q.front() << endl;
+        q.pop();
+    }
+    
+    return 0;
+  }
